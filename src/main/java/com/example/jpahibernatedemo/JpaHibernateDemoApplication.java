4c9697f4c9a2a3363181cd29135ce11e9da91324
Devansu <devansu1991@gmail.com>
2018-08-23 16:08:04 +0530

    Spring Boot Jpa Hibernate App

---
+++ src/main/java/com/example/jpahibernatedemo/JpaHibernateDemoApplication.java
@@ -0,0 +1,15 @@
+package com.example.jpahibernatedemo;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaHibernateDemoApplication{
+
+
+    public static void main(String[] args) {
+		SpringApplication.run(JpaHibernateDemoApplication.class, args);
+	}
+
+}
